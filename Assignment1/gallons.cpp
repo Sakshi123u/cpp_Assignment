5541cac4caa18e625336ea22f570c95493227402
sakshipandharkar <pandharkarsakshi2005@gmail.com>
2024-01-22 00:13:27 +0530

    added a program to calculate gallons

---
+++ Assignment1/gallons.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float gallons;
+    float cf;
+    cout<<"enter number of gallons :"<<endl;
+    cin>>gallons;
+    cf=gallons/7.481;
+    cout<<cf;
+    return 0;
+}
